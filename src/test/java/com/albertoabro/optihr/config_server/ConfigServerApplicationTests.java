bd0f8cbd8f11621a869676a95337a2081b76821b
Alberto Abad <albertoabro@gmail.com>
2024-11-11 19:13:44 +0100

    initial commit

---
+++ src/test/java/com/albertoabro/optihr/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertoabro.optihr.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
